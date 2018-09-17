49ccded95474e6715c46440be289ca1a39a04184
msdx321 <msdx321@gmail.com>
2018-09-17 15:13:57 -0400

    Improve Problem 9 again
    Signed-off-by: msdx321 <msdx321@gmail.com>

--- Project1.c
+++ Project1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 /*
 Group number:
@@ -248,11 +247,7 @@ int ques9(int x, int n, int c)
 /* answer 9 */
 int ans9(int x, int n, int c)
 {
-    // No intermediate variable created explicitly, fewer operations.
-    return (~(0xff << (n << 3)) & x) | (c << (n << 3));
-
-    // This is a more readable and with fewer operations, but there's a risk of data overflow
-    // return (x % (int)pow(2, n * 8)) + (c * pow(2, n * 8));
+    return (x & ~(0xff << (n * 8))) + (c << (n * 8));
 }
 
 
