dc06fd2cff78d3a14cfc809ed10981c7590cbd3a
marcusyounggwu <32200080+marcusyounggwu@users.noreply.github.com>
2018-09-16 22:12:16 -0400

    Update Project1.c

--- Project1.c
+++ Project1.c
@@ -350,13 +350,16 @@ int ques14(int x)
 
 /* answer 14 */
 int ans14(int x){
-  int count = 0; 
-  while(x){
+  int counter = 0; 
+  while(x)
+  {
     count += x&1;
     x >>=1;
   }
-  if (count % 2 == 1) return 1;
-  else return 0;
+  if (count % 2 == 1)
+	  return 1;
+  else 
+	  return 0;
   
 }
 
